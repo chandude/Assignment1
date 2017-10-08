4a0e59000b34347aebbf01494bf02ce14c1fc457
haro-freezd <harmanmahal@gmail.com>
2017-10-08 04:20:14 -0700

    Added formatting to the plane detail table

--- application/views/_planedetail.php
+++ application/views/_planedetail.php
@@ -1,12 +1,30 @@
 <?php
 ?>
-<td>ID: {id}</td>
-<td>Manufacturer: {manufacturer}</td>
-<td>Model: {model}</td>
-<td>Price: ${price}000</td>
-<td>Seats: {seats}</td>
-<td>Reach: {reach} km</td>
-<td>Cruising Speed: {cruise}</td>
-<td>Takeoff cost: ${takeoff}</td>
-<td>Hourly Cost: ${hourly}</td>
+<tr>
+    <td align="left">ID:</td><td align="left">{id}</td>
+</tr>
+<tr>
+    <td align="left">Manufacturer:</td><td align="left">{manufacturer}</td>
+</tr>
+<tr>
+    <td align="left">Model:</td><td align="left">{model}</td>
+</tr>
+<tr>
+    <td align="left">Price:</td><td align="left">${price}000</td>
+</tr>
+<tr>
+    <td align="left">Seats:</td><td align="left">{seats}</td>
+</tr>
+<tr>
+    <td align="left">Reach:</td><td align="left">{reach} km</td>
+</tr>
+<tr>
+    <td align="left">Cruising Speed:</td><td align="left">{cruise} km/h</td>
+</tr>
+<tr>
+    <td align="left">Takeoff cost:</td><td align="left">${takeoff}</td>
+</tr>
+<tr>
+    <td align="left">Hourly Cost:</td><td align="left">${hourly}</td>
+</tr>
 
