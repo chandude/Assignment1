4fd989c56a63e7367bea8f738beb8a9bf7df8e93
Chandu <chandu.dissanayake3@gmail.com>
2017-11-12 14:41:45 -0800

    Dynamic homepage/entity classes added

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -18,14 +18,27 @@ class Welcome extends Application
     $allPlanes = $this->fleets->all();
     $allFlights = $this->flights->all();
 
+    //Get array from wackyAPI with vulture information
+    $airline = $this->wackyAPI->getAirline();
+
+    //grab destination airport codes
+    $destinationCodes = array($airline["dest1"], $airline["dest2"], $airline["dest3"]);
+
+    $destinationNames = array();
+
+    //use WackyAPI to grab names of destinations
+    foreach($destinationCodes as $destination)
+    {
+        $destination = $this->wackyAPI->getAirport($destination);
+        array_push($destinationNames,$destination["airport"]);
+    }
+
     $totalFlights = count($allFlights);
     $totalPlanes = count($allPlanes);
-    $destinations = array('Fort St. John Airport', 'Abbotsford Airport', 'Dease Lake Airport');
-
     $this->data['flightTotal'] = $totalFlights;
     $this->data['planeTotal'] = $totalPlanes;
     $this->data['homeBase'] = 'Fort Nelson Airport';
-    $this->data['destinationAirports'] = implode('<br>', $destinations);
+    $this->data['destinationAirports'] = implode('<br>', $destinationNames);
 
     $this->data['pagetitle'] = 'Vulture Airlines';
     $this->data['pagebody'] = 'welcome_message';
